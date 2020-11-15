551bf0b98bb9f013fa6919f94e3d994775ca69fd
Rebecca <graham.cali@gmail.com>
2020-11-14 21:21:44 -0600

    commit tree b89c8b6d894ab715eea4be4133640ba50a8bd6a1

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "} # Your code here
+ end
+
+def summon_captain_planet(array)# code an argument here
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+end
+planeteer_calls
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? do |word|# Your code here
+    word.length > 4
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)# code an argument here # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
